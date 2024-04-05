c8811280fd8c56894757dc2b81838424e06ca808
Sayuri <sayurikamble123@gmail.com>
2024-04-05 20:23:37 +0530

    redux setup

---
+++ slices/navSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTimeTravelInformation: (state, action) => {
+      state.timeTravelInformation = action.payload;
+    },
+  },
+});
+
+export const {
+  setOrigin,
+  setDestination,
+  setTimeTravelInformation,
+} = navSlice.actions;
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTimeTravelInformation = (state) =>
+  state.nav.timeTravelInformation;
+
+export default navSlice.reducer;
